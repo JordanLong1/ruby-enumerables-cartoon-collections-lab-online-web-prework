2a25c765b1ffc15a883fae4820704b9c4ecf68f1
Jordan Long <jordanleelong5@gmail.com>
2019-09-28 20:41:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ puts "#{index + 1} #{name}"
   # Your code here
 end
 
-def summon_captain_planet(planeteer_calls)
-planeteer_calls.collect do |call|
+def summon_captain_planet(veggies)
+veggies.collect do |call|
   call.capitalize + "!"
   # Your code here
 end
