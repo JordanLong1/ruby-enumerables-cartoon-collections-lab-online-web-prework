05837f691857ebfb6d1a018313c1a27da1e94740
Jordan Long <jordanleelong5@gmail.com>
2019-09-28 20:12:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,13 @@
 def roll_call_dwarves(array)
-  dwarves = [""]
-  
+  new = []
+  counter = 0
+  while counter < array.length do
+    new.push(yield(array[counter]))
+    counter += 1
+  end
+  new
+  end
+
   # Your code here
 end
 
